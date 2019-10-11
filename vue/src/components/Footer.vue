ea2e64b27bdada26c68cf35c138b7dfbf4571f8a
OKU FUJISYOHI <okuxxbos20@gmail.com>
2019-10-12 03:20:12 +0900

    new component

---
+++ vue/src/components/Footer.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="footer">
+    <div class="container">
+    <ul class="nav justify-content-center mb-3 pl-5">
+      <ul class="nav flex-column col-md-2">
+        <li class="nav-item"><a class="nav-link main" href="#"><b>PRODUCT</b></a></li>
+        <li class="nav-item"><a class="nav-link sub" href="faq.html">FAQ</a></li>
+        <li class="nav-item"><a class="nav-link sub" href="#">Blog</a></li>
+      </ul>
+      <ul class="nav flex-column col-md-2">
+        <li class="nav-item"><a class="nav-link main" href="#"><b>ABOUT US</b></a></li>
+        <li class="nav-item"><a class="nav-link sub" href="#">Get in touch</a></li>
+        <li class="nav-item"><a class="nav-link sub" href="#">Support</a></li>
+      </ul>
+      <ul class="nav flex-column col-md-2">
+        <li class="nav-item"><a class="nav-link main" href="#"><b>HOW TO</b></a></li>
+        <li class="nav-item"><a class="nav-link sub" href="#">Install</a></li>
+        <li class="nav-item"><a class="nav-link sub" href="#">Set up</a></li>
+      </ul>
+      <ul class="nav flex-column col-md-2">
+        <li class="nav-item"><a class="nav-link main" href="#"><b>Comunity</b></a></li>
+        <li class="nav-item"><a class="nav-link sub" href="#">Terms</a></li>
+        <li class="nav-item"><a class="nav-link sub" href="#">Privacy</a></li>
+      </ul>
+    </ul>
+    <ul class="footer-sns text-center mt-5" style="padding: 0;">
+      <a href="#"><i class="fab fa-facebook-f" style="margin-right: 30px;"></i></a>
+      <a href="#"><i class="fab fa-twitter" style="margin-right: 30px;"></i></a>
+      <a href="#"><i class="fab fa-instagram" style="margin-right: 30px;"></i></a>
+      <a href="#"><i class="fab fa-gitlab" style="margin-right: 30px;"></i></a>
+    </ul>
+    <p class="text-center mb-0"><small>Copylight &copy;2019 PaperAirplane, All Rights Reserved.</small></p>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
+
+.footer {
+  background-color: var(--subColor);
+  /* position: fixed; */
+  width: 100%;
+  color: var(--white);
+  padding-top: 30px;
+}
+
+.main {
+  cursor: pointer;
+  transition: 0.4s;
+}
+
+.main:hover {
+  transform: scale(1.1)
+}
+
+footer a, footer a:hover {
+  color: #fff;
+}
+
+.sub {
+  font-size: 13px;
+  transition: 0.2s;
+}
+
+.sub:hover {
+  color: #6c7a89;
+}
+
+footer i {
+  font-size: 25px;
+  transition: 0.3s;
+}
+
+footer i:hover {
+  color: #fff;
+  transform: scale(1.3)
+}
+
+</style>
---
+++ vue/src/components/Header.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="header">
+  <b-navbar toggleable="lg">
+    <b-navbar-brand href="#" class="title"><b>PaperAirplane</b></b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#" class="item">PRODUCT</b-nav-item>
+        <b-nav-item href="#" class="item">ABOUT US</b-nav-item>
+        <b-nav-item href="#" class="item">HOW TO</b-nav-item>
+        <b-nav-item href="#" class="item">COMUNITY</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">日本語</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template v-slot:button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+      </b-navbar-nav>
+
+    </b-collapse>
+  </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
+
+.header {
+  background-color: var(--mainColor);
+  position: fixed;
+  width: 100%;
+}
+
+.item {
+  color: #fff;
+}
+
+.title {
+  font-family: 'Open Sans', sans-serif;
+  color: var(--white);
+}
+
+.title:hover {
+  font-family: 'Open Sans', sans-serif;
+  color: #fff;
+}
+</style>
