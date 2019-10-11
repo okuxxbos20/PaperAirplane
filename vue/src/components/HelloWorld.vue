cfae44d0e7ea677dc14fe5451da9290685fa764a
OKU FUJISYOHI <okuxxbos20@gmail.com>
2019-10-12 03:25:01 +0900

    create space

--- vue/src/components/HelloWorld.vue
+++ vue/src/components/HelloWorld.vue
@@ -44,15 +44,19 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
+  text-decoration: none;
 }
 </style>
