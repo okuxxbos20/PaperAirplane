e679c243840cd3f0988e2e0572f2b0646c420666
OKU FUJISYOHI <okuxxbos20@gmail.com>
2019-10-12 03:23:16 +0900

    import defined colors

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -3,27 +3,43 @@
     <Header/>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header,
+    Footer
   }
 }
 </script>
 
 <style>
+:root {
+  --mainColor: #19b5fe;
+  --subColor: #3bb4c1;
+  --black: #2e3131;
+  --white: #fff;
+}
+
 #app {
+  margin: 0;
+  padding: 0;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+img {
+  margin-top: 100px;
 }
 </style>
