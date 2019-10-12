7b1b4ad0dc730d91630615fc70c15878aa3696c1
OKU FUJISYOHI <okuxxbos20@gmail.com>
2019-10-12 18:48:25 +0900

    delete space

--- vue/src/components/Header.vue
+++ vue/src/components/Header.vue
@@ -3,7 +3,6 @@
   <b-navbar toggleable="lg" type="dark">
     <b-navbar-brand href="#" class="title"><b>PaperAirplane</b></b-navbar-brand>
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
         <b-nav-item href="#" class="item" active>PRODUCT</b-nav-item>
@@ -11,15 +10,12 @@
         <b-nav-item href="#" class="item" active>HOW TO</b-nav-item>
         <b-nav-item href="#" class="item" active>COMUNITY</b-nav-item>
       </b-navbar-nav>
-
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-
-        <b-nav-item-dropdown text="Lang" right>
+        <b-nav-item-dropdown text="Lang" active>
           <b-dropdown-item href="#">EN</b-dropdown-item>
           <b-dropdown-item href="#">日本語</b-dropdown-item>
         </b-nav-item-dropdown>
-
         <b-nav-item-dropdown right>
           <!-- Using 'button-content' slot -->
           <template v-slot:button-content>
@@ -28,9 +24,7 @@
           <b-dropdown-item href="#">Profile</b-dropdown-item>
           <b-dropdown-item href="#">Sign Out</b-dropdown-item>
         </b-nav-item-dropdown>
-
       </b-navbar-nav>
-
     </b-collapse>
   </b-navbar>
   </div>
