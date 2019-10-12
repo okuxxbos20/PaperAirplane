bd6f6d5495b2c01db0fd058382ebfd1f83ab30c9
OKU FUJISYOHI <okuxxbos20@gmail.com>
2019-10-12 18:11:12 +0900

    delete css at "#app"

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -33,9 +33,6 @@ export default {
 #app {
   margin: 0;
   padding: 0;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
 }
 
