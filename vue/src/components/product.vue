ca610896c6586b6599635a61a3b48d7ba0098b21
OKU FUJISYOHI <okuxxbos20@gmail.com>
2019-10-18 21:54:36 +0900

    new file

---
+++ vue/src/components/product.vue
@@ -0,0 +1,24 @@
+<!-- <template>
+  <p>this is product.vue</p>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style lang="scss">
+p {
+  color: red;
+}
+</style> -->
