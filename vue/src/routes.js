cb2c836b68cb1796be126683514f8eef2de738d3
OKU FUJISYOHI <okuxxbos20@gmail.com>
2019-10-28 17:41:49 +0900

    add index component

--- vue/src/routes.js
+++ vue/src/routes.js
@@ -1,10 +1,14 @@
-// import IndexComp from './components/index';
+import IndexComp from './components/index';
 import ProductComp from './components/product';
 import AboutusComp from './components/aboutus';
 import HowtoComp from './components/howto';
 import ComunityComp from './components/comunity';
 
 export const routes = [{
+    path: '/',
+    component: IndexComp,
+    name:'index'
+  },{
     path: '/product',
     component: ProductComp,
     name:'product'
